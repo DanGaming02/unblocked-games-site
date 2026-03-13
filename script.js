4eb565b4dd50cc76cdfc9948596038468336f67d
DanGaming02 <dangaming3555@gmail.com>
2026-03-13 07:58:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Default thumbnail if a game has no image
 const DEFAULT_THUMBNAIL = "default-thumbnail.png";
 
 fetch("games.json")
-.then(response => response.json())
+.then(res => res.json())
 .then(games => {
 
 const container = document.getElementById("games");
@@ -12,32 +11,32 @@ games.forEach(game => {
 const div = document.createElement("div");
 div.className = "game";
 
-// choose thumbnail or default
-const thumb = game.thumbnail && game.thumbnail !== ""
-? game.thumbnail
-: DEFAULT_THUMBNAIL;
+const thumb = game.thumbnail || DEFAULT_THUMBNAIL;
 
 div.innerHTML = `
-<img src="${thumb}" alt="${game.name}">
+<img src="${thumb}">
 <p>${game.name}</p>
 `;
 
-// open game through play.html
 div.onclick = () => {
 
-const encodedURL = encodeURIComponent(game.path);
+if(game.type === "html"){
 
-window.location.href = `play.html?game=${encodedURL}`;
+window.location.href = game.path;
+
+}
+
+else if(game.type === "url" || game.type === "iframe"){
+
+const encoded = encodeURIComponent(game.path);
+window.location.href = `play.html?game=${encoded}`;
+
+}
 
 };
 
 container.appendChild(div);
 
 });
-
-})
-.catch(error => {
-
-console.error("Failed to load games.json:", error);
 
 });
